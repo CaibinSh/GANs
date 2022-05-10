d27164144c156ab5f9f4864cbd4b58827ba1296c
Sheng, Caibin <caibin.sheng@novartis.com>
2022-05-11 01:36:57 +0200

    init: initial uploading

---
+++ gans/test/test_generator.py
@@ -0,0 +1,57 @@
+""" This tests the generator function. """
+from cgi import test
+import decimal
+import unittest
+import numpy
+import torch
+from decimal import Decimal
+from ..generator import get_generator_block, generator
+
+
+class generator_test(unittest.TestCase):
+    """
+    Test generator.py
+    """
+
+    def test_generator_block(self):
+        """
+        Test generator()
+        """
+        in_features, out_features, num_test=40, 20, 1000
+
+        gen = get_generator_block(in_features, out_features)
+
+        self.assertTrue(len(gen)==3, "block number is not 3")
+        self.assertIsInstance(gen[0], nn.Linear, "1st block is not Linear")
+        self.assertIsInstance(gen[1], nn.BatchNorm1d, "2nd block is not BatchNorm")
+        self.assertIsInstance(gen[2], nn.ReLU, "3rd block is not ReLU")
+        
+        test_input = torch.randn(in_features, out_features)
+        test_output = get_generator_block(test_input)
+        self.assertTrue(np.array_equal(test_output.shape, (num_test, out_features)))
+
+        self.assertGreater(test_output.std(), .55)
+        self.assertLess(test_output.std(), .65)
+    
+    def test_generator(self):
+        """
+        Test generator()
+        """
+
+        z_dim, im_dim, hidden_dim, num_test = 5, 10, 20, 10000
+
+        gen = generator(z_dim, im_dim, hidden_dim).get_gen()
+
+        self.assertTrue(len(gen)==6, "block number is not 6")
+
+        test_input = torch.randn(num_test, z_dim)
+        test_output = gen(test_input)
+
+        self.assertTrue(np.array_equal(test_output.shape, (num_test, im_dim)))
+
+        self.assertGreater(test_output.std(), .55)
+        self.assertLess(test_output.std(), .65)
+        
+  
+if __name__ == '__main__':
+    unittest.main()
