a04d76d57b39ec16fb8e1d89d05431906e69b76b
Sheng, Caibin <caibin.sheng@novartis.com>
2022-05-11 10:40:17 +0200

    test(test): fix a bug in test

--- gans/test/test_generator.py
+++ gans/test/test_generator.py
@@ -1,11 +1,22 @@
 """ This tests the generator function. """
-from cgi import test
-import decimal
+import sys
+from pathlib import Path
 import unittest
-import numpy
+import numpy as np
 import torch
-from decimal import Decimal
-from ..generator import get_generator_block, generator
+import torch.nn as nn
+
+
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+# Additionally remove the current file's directory from sys.path
+try:
+    sys.path.remove(str(parent))
+except ValueError: # Already removed
+    pass
+
+from generator import get_generator_block, generator
 
 
 class generator_test(unittest.TestCase):
@@ -19,15 +30,15 @@ class generator_test(unittest.TestCase):
         """
         in_features, out_features, num_test=40, 20, 1000
 
-        gen = get_generator_block(in_features, out_features)
+        gen_block = get_generator_block(in_features, out_features)
 
-        self.assertTrue(len(gen)==3, "block number is not 3")
-        self.assertIsInstance(gen[0], nn.Linear, "1st block is not Linear")
-        self.assertIsInstance(gen[1], nn.BatchNorm1d, "2nd block is not BatchNorm")
-        self.assertIsInstance(gen[2], nn.ReLU, "3rd block is not ReLU")
+        self.assertTrue(len(gen_block)==3, "block number is not 3")
+        self.assertIsInstance(gen_block[0], nn.Linear, "1st block is not Linear")
+        self.assertIsInstance(gen_block[1], nn.BatchNorm1d, "2nd block is not BatchNorm")
+        self.assertIsInstance(gen_block[2], nn.ReLU, "3rd block is not ReLU")
         
-        test_input = torch.randn(in_features, out_features)
-        test_output = get_generator_block(test_input)
+        test_input = torch.randn(num_test, in_features)
+        test_output = gen_block(test_input)
         self.assertTrue(np.array_equal(test_output.shape, (num_test, out_features)))
 
         self.assertGreater(test_output.std(), .55)
@@ -48,9 +59,10 @@ class generator_test(unittest.TestCase):
         test_output = gen(test_input)
 
         self.assertTrue(np.array_equal(test_output.shape, (num_test, im_dim)))
-
-        self.assertGreater(test_output.std(), .55)
-        self.assertLess(test_output.std(), .65)
+        
+        self.assertLess(test_output.min(), .5, "Don't use a block in your solution")
+        self.assertGreater(test_output.std(), .05, "Don't use batchnorm here")
+        self.assertLess(test_output.std(), .15, "Don't use batchnorm here")
         
   
 if __name__ == '__main__':
